973783ee60bb6533a217842737b020a67a48cfeb
peturik <38261253+peturik@users.noreply.github.com>
2021-10-23 13:53:20 +0200

    Update my.js

--- my.js
+++ my.js
@@ -7,3 +7,22 @@
   $.each(obj, function (index, value) {
     console.log(value);
   });
+
+/*
+forEach
+Метод «arr.forEach(callback[, thisArg])» используется для перебора массива.
+
+Он для каждого элемента массива вызывает функцию callback.
+
+Этой функции он передаёт три параметра callback(item, i, arr):
+
+item – очередной элемент массива.
+i – его номер.
+arr – массив, который перебирается.
+Например:
+*/
+var arr = ["Яблоко", "Апельсин", "Груша"];
+
+arr.forEach(function(item, i, arr) {
+  alert( i + ": " + item + " (массив:" + arr + ")" );
+});
