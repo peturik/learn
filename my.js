6f129ae2a7cc6075224f560bd49017e5b5ada088
peturik <38261253+peturik@users.noreply.github.com>
2021-10-23 14:16:17 +0200

    Update my.js

--- my.js
+++ my.js
@@ -1,11 +1,11 @@
   //function each for document jquery
   $( "li" ).each(function( index ) {
-    console.log( index + ": " + $( this ).text() );
+    //console.log( index + ": " + $( this ).text() );
   });
   
   //function each for script jquery
   $.each(obj, function (index, value) {
-    console.log(value);
+    //console.log(value);
   });
 
 /*
